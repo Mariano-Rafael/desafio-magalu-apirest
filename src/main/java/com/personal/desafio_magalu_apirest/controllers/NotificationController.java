4d7dd256527e0238fba5b3637dc651c70e0043df
Rafael Mariano <rmariano38@gmail.com>
2025-03-01 16:31:11 -0300

    desafio finalizado, sem exceptions

--- src/main/java/com/personal/desafio_magalu_apirest/controllers/NotificationController.java
+++ src/main/java/com/personal/desafio_magalu_apirest/controllers/NotificationController.java
@@ -1,12 +1,12 @@
 package com.personal.desafio_magalu_apirest.controllers;
 
 import com.personal.desafio_magalu_apirest.dtos.NotificationDTO;
+import com.personal.desafio_magalu_apirest.entities.Notification;
 import com.personal.desafio_magalu_apirest.services.NotificationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/notification")
@@ -24,5 +24,21 @@ public class NotificationController {
 
         return ResponseEntity.ok().build();
 
+    }
+
+    @GetMapping("/{notificationId}")
+    public ResponseEntity<Notification> getNotification(@PathVariable("notificationId") Long notificationId) {
+
+        Optional<Notification> notification = notificationService.findById(notificationId);
+
+        return notification.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{notificationId}")
+    public ResponseEntity<Void> cancelNotification(@PathVariable("notificationId") Long notificationId) {
+
+        notificationService.cancelNotificationById(notificationId);
+
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/personal/desafio_magalu_apirest/services/NotificationService.java
+++ src/main/java/com/personal/desafio_magalu_apirest/services/NotificationService.java
@@ -1,8 +1,12 @@
 package com.personal.desafio_magalu_apirest.services;
 
 import com.personal.desafio_magalu_apirest.dtos.NotificationDTO;
+import com.personal.desafio_magalu_apirest.entities.Notification;
+import com.personal.desafio_magalu_apirest.entities.enums.StatusEnum;
 import com.personal.desafio_magalu_apirest.repositories.NotificationRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class NotificationService {
@@ -16,5 +20,21 @@ public class NotificationService {
     public void saveNotification(NotificationDTO notificationDTO) {
 
         notificationRepository.save(notificationDTO.toNotification());
-}
+    }
+
+    public Optional<Notification> findById(Long notificationId) {
+        return notificationRepository.findById(notificationId);
+    }
+
+    public void cancelNotificationById(Long notificationId) {
+        Optional<Notification> notification = findById(notificationId);
+
+        if (notification.isPresent()) {
+
+            notification.get().setStatusEnum(StatusEnum.CANCELED);
+
+            notificationRepository.save(notification.get());
+        }
+
+    }
 }
