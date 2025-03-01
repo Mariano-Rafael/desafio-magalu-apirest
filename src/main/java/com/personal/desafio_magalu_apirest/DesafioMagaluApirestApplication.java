2e78432fbd7270b50d37598411f1183590d30b34
Rafael Mariano <rmariano38@gmail.com>
2025-03-01 11:01:52 -0300

    first commit

---
+++ src/main/java/com/personal/desafio_magalu_apirest/DesafioMagaluApirestApplication.java
@@ -0,0 +1,13 @@
+package com.personal.desafio_magalu_apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMagaluApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMagaluApirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/desafio_magalu_apirest/DesafioMagaluApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.desafio_magalu_apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMagaluApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
